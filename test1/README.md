4214f414230fb8ddc85b79ce603b25704d1354f3
Moamaldev <90430221+Moamaldev@users.noreply.github.com>
2021-09-25 13:07:48 +0300

    Delete test1 directory

--- test1/README.md
+++
@@ -1,2 +0,0 @@
-
-the solation in library bin 
--- test1/test/test1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:test1/test1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
